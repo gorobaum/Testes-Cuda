c83103e8d68c7c101967e19dd82eef99a9c6251f
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-07-27 13:18:36 -0300

    Ta funfando a multiplicação! vish vish

--- processbound/process.cu
+++ processbound/process.cu
@@ -4,30 +4,34 @@
 #include <stdio.h>
 #include <math.h>
 
-__global__ void MatrixCopy (double* MatrixA, double* MatrixB, int row, int column) {
-  int i = blockIdx.x*blockDim.x+threadIdx.x;
-  int j = blockIdx.y*blockDim.y+threadIdx.y;
-  MatrixB[i*column+j] = MatrixA[i*column+j];
+__global__ void MatrixCopy (double* MatrixA, double* MatrixB, double* MatrixC, int N) {
+  int j = blockIdx.x*blockDim.x+threadIdx.x;
+  int i = blockIdx.y*blockDim.y+threadIdx.y;
+  int k;
+
+  for (k = 0; k < N; k++ )
+    MatrixC[i*N+j] += MatrixA[i*N+k]*MatrixB[k+j*N];
 }
 
 int main () {
-  int row = 100,
-      column = 100,
+  int N = 32,
       i = 0,
       j = 0;
   dim3 threadPerBlock(16, 16),
-       blocksPerGrid(row/threadPerBlock.x+1, column/threadPerBlock.y+1);
-  size_t size = row*column*sizeof(double);
-  double *MatrixA, *MatrixB, *cudaMA, *cudaMB;
+       blocksPerGrid(N/threadPerBlock.x, N/threadPerBlock.y);
+  size_t size = N*N*sizeof(double);
+  double *MatrixA, *MatrixB, *MatrixC, *cudaMA, *cudaMB, *cudaMC;
   
   /* Matrix allocation. */
   MatrixA = (double*)malloc(size);
   MatrixB = (double*)malloc(size);
-  
-  for (i = 0; i < row; i++)
-    for (j = 0; j < column; j++) {
-      MatrixA[i*column+j] = i*column+j;
-      MatrixB[i*column+j] = 2.0;
+  MatrixC = (double*)malloc(size);
+ 
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++) {
+      MatrixA[i*N+j] = i*N+j;
+      MatrixB[i*N+j] = 2.0;
+      MatrixC[i*N+j] = 0.0;
     }
  
   /* Cuda memory allocation. */
@@ -35,23 +39,27 @@ int main () {
       printf("Erro na alçocação de recursos!\n");
   if (cudaMalloc(&cudaMB, size) != cudaSuccess)
       printf("Erro na alçocação de recursos!\n");
+  if (cudaMalloc(&cudaMC, size) != cudaSuccess)
+      printf("Erro na alçocação de recursos!\n");
 
   /* Cuda memory copy. */
   if (cudaMemcpy(cudaMA, MatrixA, size, cudaMemcpyHostToDevice) != cudaSuccess)
       printf("Erro na cópia de recursos!\n");
   if (cudaMemcpy(cudaMB, MatrixB, size, cudaMemcpyHostToDevice) != cudaSuccess)
       printf("Erro na cópia de recursos!\n");
+  if (cudaMemcpy(cudaMC, MatrixC, size, cudaMemcpyHostToDevice) != cudaSuccess)
+      printf("Erro na cópia de recursos!\n");
 
   /* Cuda kernel call. */
-  MatrixCopy<<<blocksPerGrid, threadPerBlock>>>(cudaMA, cudaMB, row, column);
+  MatrixCopy<<<blocksPerGrid, threadPerBlock>>>(cudaMA, cudaMB, cudaMC, N);
   cudaDeviceSynchronize();
 
-  if (cudaMemcpy(MatrixB, cudaMB, size, cudaMemcpyDeviceToHost) != cudaSuccess)
+  if (cudaMemcpy(MatrixC, cudaMC, size, cudaMemcpyDeviceToHost) != cudaSuccess)
       printf("Erro na cópia do Device para o Host!\n");
 
-  for (i = 0; i < row; i++) {
-    for (j = 0; j < column; j++) {
-      printf("%5.lf\n", MatrixB[i*column+j]);
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      printf("%5.lf\n", MatrixC[i*N+j]);
     }
     printf("\n");
   }
